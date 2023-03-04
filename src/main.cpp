668d32d29d700c80fb0e32962be8cfdf2208ad5d
kittitach1526 <fanta.kittitach@gmail.com>
2023-03-04 16:15:18 +0700

    hot fix

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ void setup() {
   //customKeypad.begin( );
   //swb.setpin_switch(25,27,12);//w,b,y
   //oled.intit_display();
-  Serial.println("Version : 0.3 wifi Scan + eeprom");
+  Serial.println("\nVersion : 0.3 wifi Scan + eeprom");
   w.BeginEEP();
   w.check_eeprom_wifi();
   //swb.on_led();
