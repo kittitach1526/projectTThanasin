863ce946df2a56b6e3b8668a23d59f3b713d2a04
kittitach1526 <fanta.kittitach@gmail.com>
2023-03-12 00:26:39 +0700

    hot fix

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ void setup() {
   //customKeypad.begin( );
   //swb.setpin_switch(25,27,12);//w,b,y
   //oled.intit_display();
-  Serial.println("\nVersion : 0.5 wifi+http ");
+  Serial.println("\nVersion : 0.6 ..... ");
   //test-3-
   w.BeginEEP();
   //w.clearEEPROM();
