9d868bc0523314cb4ab086a5d6c49b4cb634b913
kittitach1526 <fanta.kittitach@gmail.com>
2023-03-11 19:45:11 +0700

    hot fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 #include <Arduino.h>
-
 #include "wifi_lib.h"
-#include "htttp_box.h"
 #include "Switch_box.h"
 #include "display_box.h"
 #include <Wire.h>
 #include "rfid_state.h"
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 //#include <rdm6300.h>
 //#define RDM6300_RX_PIN  16
 //Rdm6300 rdm6300;
 String msg = " Code :";
 byte state=0;
-
+String getdataJson(String key_search);
 #include <Keypad_I2C.h>
 #include <Keypad.h>
 #include <Wire.h>
-
+HTTPClient http;
 #define I2CADDR 0x20
 
 const byte ROWS = 4;
@@ -42,14 +42,16 @@ Keypad_I2C customKeypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS, I2CADDR
   white switch pin = 25;
   oled = display_box.h
 */
+String serverUrl ="https://bunnam.com/projects/majorette_pp/update/quit_v3.php?id_mc=02-02&id_rfid=0004953220&id_activity=4539&activity_type=1&no_send=1&no_pulse1=300&no_pulse2=1&no_pulse3=100&multiplier=1";
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
-  
+  w.ssid = "Best&Man_2.4G-wlan";
+  w.password ="best5261";
+  w.connect_wifi();
   //w.ssid = "Best&Man_2.4G-wlan";
   //w.password = "best5261";
-  //http_b.server_url ="http://20.231.75.176:1880/";
   //customKeypad.begin( );
   //swb.setpin_switch(25,27,12);//w,b,y
   //oled.intit_display();
@@ -63,18 +65,59 @@ void setup() {
   //w.connect_wifi();
   //oled.show(1," Code by Glenda");
   //rdm6300.begin(RDM6300_RX_PIN);
-  rf_st.Init_rfid();
+  //rf_st.Init_rfid();
+  
   Serial.println("All Setup Complete!"); 
   state =0;
-
-
 }
 
 void loop() { 
-  rf_st.read_rfid();
-  Serial.print("RFID : ");
-  Serial.println(rf_st.result_rfid);
-  delay(500);
+  
+  String keydata = getdataJson("code");
+  Serial.println("code : "+keydata);
+
+  delay(5000);
+}
+
+String getdataJson(String key_search)
+{
+  http.begin(serverUrl);
+  int httpResponseCode = http.GET();
+
+  if (httpResponseCode == 200) {
+    String payload = http.getString();
+    Serial.print("Json : ");
+    Serial.println(payload);
+
+    // Parse JSON object
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return "error";
+      //return;
+    }
+    // Extract values
+    //const char* title = doc["title"];
+    //bool completed = doc["completed"];
+    String getdata = doc[key_search];
+    Serial.println("Value frome key : "+getdata);
+    return getdata;
+  } 
+  else {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+}
+
+  //rf_st.read_rfid();
+  //Serial.print("RFID : ");
+  //Serial.println(rf_st.result_rfid);
+  //delay(500);
+
+
   /*
   if(state == 0)
   {
@@ -153,6 +196,6 @@ void loop() {
       oled.drawStr(0,0,msg);
     }
   }*/
-}
+
 
 
