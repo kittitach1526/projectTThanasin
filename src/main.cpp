5014026ea840c4fb0a52fec861e5688b4d92ece7
kititach1526 <fanta.kittitach@gmail.com>
2023-04-22 14:35:44 +0700

    ้hot fix

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,14 @@ String getdataJson(String key_search);
 #include <Wire.h>
 HTTPClient http;
 
-
+typedef struct userdata
+{
+    String rfid="";
+    String name="";
+    String lastname="";
+    String work="";
+    String mcnumber="01";
+}userdata;
 /*
   w = wifi_lib.h
   http_b = htttp_box.h
@@ -45,20 +52,25 @@ void setup() {
   oled.clear();
   Serial.println("\nVersion : 0.8 api ");
   //test-3-
-  //w.BeginEEP();
+  w.BeginEEP();
   //w.clearEEPROM();
-  //w.check_eeprom_wifi();
+  w.check_eeprom_wifi();
   //w.searchWiFi();
   //swb.on_led();
   //rf_st.Init_rfid();
   //w.searchWiFi();
   //w.connect_wifi();
   //rdm6300.begin(RDM6300_RX_PIN);
-  //rf_st.Init_rfid();
+  rf_st.Init_rfid();
   Serial.println("All Setup Complete!"); 
   state =0;
+  
 }
+/*-----------------------------------------------------------------------------------------------------------*/
+userdata staff;
 
+
+/*-----------------------------------------------------------------------------------------------------------*/
 void loop() { 
   //char data = kp.readkeypad();
   //Serial.println(data);
@@ -76,7 +88,25 @@ void loop() {
     }
   }
   //delay(5000);*/
+  if(state == 0)
+  {
+    while(staff.rfid ="")
+    {
+      if(Serial.available()>0)
+      {
+        String data = Serial.readString();
+        staff.rfid=data;
+      }
+    }
+    Serial.println("RFID : "+staff.rfid);
+    state = 1 ;
+  }
+  if(state == 1)
+  {
+    
+  }
 }
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 String getValue(String data, char separator, int index)
 {
     int found = 0;
@@ -125,91 +155,6 @@ String getdataJson(String key_search)
   }
   http.end();
 }
-
-  //rf_st.read_rfid();
-  //Serial.print("RFID : ");
-  //Serial.println(rf_st.result_rfid);
-  //delay(500);
-
-
-  /*
-  if(state == 0)
-  {
-    while (Serial.available()<= 0 )
-    {
-        Serial.println(" Enter RFID : ");
-        delay(5000);
-    }
-    if(Serial.available() > 0)
-    {
-      String data_serial = Serial.readString();
-      Serial.println("RFID : "+data_serial);
-      //http_b.post("update-sensor","temperature="+data_serial);//test
-      state =1;
-    }
-    //http_b.post("update-sensor","temperature=20");
-    //delay(1000);
-    //state =1;
-  }
-  if(state == 1 )
-  {
-    String response = http_b.recive();
-    //response.trim();
-    Serial.println(response);
-  }
-  /*
-  if(state ==3 )
-  {
-    Serial.println("working on state 1");
-    oled.show(1," เลือกระบบการทำงาน");
-    oled.show(2," สีฟ้า  = BF");
-    oled.show(3," สีส้ม = RW");
-    oled.show(4," สีขาว = DT");
-    state =4 ;
-  }
-  if(state ==4 )
-  {
-    swb.read_all_switch();
-    if(swb.result_blue_switch == 0)
-      {
-        Serial.println("go to bf state !");
-        oled.clear();
-        oled.show(1,"now is BF state");
-        state =5 ;
-      }
-    if(swb.result_white_switch == 0)
-      {
-        Serial.println("go to dt state !");
-        oled.clear();
-        oled.show(1,"now is DT state");
-        oled.show(2," Code : ");
-        oled.clear();
-        state = 6;
-      }
-    if(swb.backup_result_yellow_switch == 0)
-    {
-      Serial.println("go to rw state !");
-      oled.clear();
-      oled.show(1,"now is RW state");
-      state == 7;
-    }
-  }
-  if(state ==4 )
-  {
-
-  }
-  if(state == 7)
-  {
-    char customKey = customKeypad.getKey();
-    oled.drawStr(0,0,msg);
-    if (customKey != NO_KEY)
-    {
-      Serial.println(customKey);
-      msg += customKey;
-      Serial.println(msg);
-      oled.drawStr(0,0,msg);
-    }
-  }*/
 
 
 
