2d380ed51f85413e3841c0fb966f41f5908d3dab
kititach1526 <fanta.kittitach@gmail.com>
2023-05-03 16:37:12 +0700

    hot fix

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 byte state=0;
 String getdataJson();
 String getValue(String data, char separator, int index);
-
+void api_contact();
 
 String mc_number = "mc200";
 
@@ -40,7 +40,7 @@ typedef struct dataStaff
 
 String serverUrl ="https://bunnam.com/projects/majorette_pp/update/quit_v3.php?id_mc=02-02&id_rfid=0004953220&id_activity=4539&activity_type=1&no_send=1&no_pulse1=300&no_pulse2=1&no_pulse3=100&multiplier=1";
 HTTPClient http;
-//String Nodered_2 = http://20.231.75.176:1880/touch?id_mc=mc200&rfid=1165304621
+String Nodered_2 = "http://20.231.75.176:1880/touch?id_mc=mc200&rfid=1165304621";
 
 void setup() {
   Wire.begin();
@@ -58,7 +58,8 @@ void setup() {
   //swb.on_led();
   rf_st.Init_rfid();
   Serial.println("All Setup Complete!"); 
-  getdataJson(); // test json funtion 
+  //getdataJson(); // test json funtion 
+  //api_contact();
   state =0;
   
 
@@ -95,6 +96,13 @@ void loop() {
     oled.clear();
     state = 1 ;
     
+  }
+  if(state == 1)
+  {
+    api_contact();
+    state =2;
+    Serial.println(" Go to > State 2 ");
+    oled.showString(1,"Go to > State 2 ");
   }
 
 }
@@ -144,4 +152,35 @@ String getdataJson()
   delay(1000);
   http.end();
 }
+}
+
+void api_contact()
+{
+  String test_server = "http://20.231.75.176:1880/touch?id_mc="+mc_number+"&rfid="+rf_st.result_rfid;
+  http.begin(test_server);
+  int httpCode = http.GET();
+
+  // Check HTTP status code
+  if (httpCode > 0) {
+    Serial.printf("HTTP status code: %d\n", httpCode);
+
+    // Check if response is available
+    if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+      String payload = http.getString();
+      payload = getValue(payload,'[',1);
+      payload = getValue(payload,']',0);
+      Serial.println(payload);
+      DynamicJsonDocument doc(2048);
+      //StaticJsonDocument<256> doc;
+      deserializeJson(doc, payload);
+      int getdata = doc["job_id"];
+      Serial.println("Value frome key : "+String(getdata));
+      //Serial.println(payload);
+    }
+  } else {
+    Serial.println("Error in HTTP request");
+  }
+
+  // Wait for 5 seconds before sending the next request
+  //delay(5000);
 }
