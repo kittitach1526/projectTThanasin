74bad1f1f55ad69f8dee413fd84fe8faf8e8b8e0
kititach1526 <fanta.kittitach@gmail.com>
2023-05-31 21:11:41 +0700

    hot fix

---
+++ include/read_json.h
@@ -0,0 +1,14 @@
+#ifndef read_json_H
+#define read_json_H
+#include <Arduino.h>
+class read_json
+{
+    public:
+        read_json();
+
+    private:
+
+
+};
+extern read_json rj;
+#endif
---
+++ src/read_json.cpp
@@ -0,0 +1,8 @@
+#include "read_json.h"
+read_json rj;
+
+
+read_json::read_json()
+{
+    
+}
